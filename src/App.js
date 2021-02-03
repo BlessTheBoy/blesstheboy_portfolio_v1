357b04b9cb8f862746490db5765ca29a5200a3f7
Faruq Bakare <bakarefaruq.a@gmail.com>
2021-02-03 01:31:26 +0100

    basic projects page setup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ import { useStateValue } from "./components/StateProvider";
 import { LandingPage } from "./components/pages/LandingPage";
 import { RecentProjects } from "./components/pages/RecentProjects";
 import { ContactPage } from "./components/pages/ContactPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+} from "react-router-dom";
+import { Projects } from "./components/pages/Projects";
 
 function App() {
   const [{ theme }] = useStateValue();
@@ -16,9 +23,18 @@ function App() {
     <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
       <GlobalStyles />
       <div className="App">
-        <LandingPage />
-        <RecentProjects />
-        <ContactPage />
+        <Router>
+          <Switch>
+            <Route path="/allprojects">
+              <Projects />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+              <RecentProjects />
+              <ContactPage />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </ThemeProvider>
   );
